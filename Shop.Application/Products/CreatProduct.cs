54817f415258a9b6e3896dc5bd2550b3c7e40b7b
mhdzam <intell_progr_1991@hotmail.com>
2020-07-15 01:52:38 +0400

    add DBContext & create Item

--- Shop.Application/Products/CreatProduct.cs
+++ Shop.Application/Products/CreatProduct.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace Shop.Application.Products
 {
     class CreatProduct
     {
+        private ApplicationDbContext _context;
+
+        public CreatProduct(ApplicationDbContext Context)
+        {
+            _context = Context;
+        }
+
+        public void Do(Product _product)
+        {
+            _context.Add(_product);
+            _context.SaveChanges();
+        }
     }
 }
