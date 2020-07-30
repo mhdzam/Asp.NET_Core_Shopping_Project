c0d145e0caaf7bf44da478a8853e4ab570c1209f
mhdzam <intell_progr_1991@hotmail.com>
2020-07-31 01:35:41 +0400

    Display Products

--- Shop.Application/Products/GetProduct.cs
+++ Shop.Application/Products/GetProduct.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Database;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Shop.Application.Products
 {
-    class GetProduct
+   public class GetProduct
     {
+        private ApplicationDbContext _ctx;
+
+        public GetProduct(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public ProductViewModel Do(string name) =>
+
+            _ctx.Products.Where(X=>X.Name == name)
+            .Include(X => X.Stock)
+            .Select(X => new ProductViewModel()
+            {
+                Id = X.Id,
+                Name = X.Name,
+                Value = X.Value,
+                 Stock = X.Stock.Select(y => new StockViewModel { 
+                  Description = y.Description,
+                   Id = y.Id,
+                    InStock = y.Qty > 0
+                 })
+            })
+            .FirstOrDefault();
+
+        public class ProductViewModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            //    public string Description { get; set; }
+            public decimal Value { get; set; }
+
+            public IEnumerable<StockViewModel> Stock { get; set; }
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public bool InStock { get; set; }
+        }
     }
 }
---
+++ ShopUI/Pages/Product.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.Products;
+using Shop.Application.StockAdmin;
+using Shop.Database;
+
+namespace ShopUI.Pages
+{
+    public class ProductModel : PageModel
+    {
+
+        private ApplicationDbContext _ctx;
+
+        public ProductModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public GetProduct.ProductViewModel Product { get; set; }
+
+        public IActionResult OnGet(string name)
+        {
+            Product = new GetProduct(_ctx).Do(name);
+            if (Product == null)
+                return RedirectToPage("Index");
+            else
+                return Page();
+        }
+    }
+}
